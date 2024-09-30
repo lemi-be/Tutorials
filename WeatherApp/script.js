6077f9214894224e9f2669e4c98750bf79bc5892
Lemi Bekele <lemibekele365@gmail.com>
2024-09-30 15:38:00 +0300

    Adding display function

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -7,12 +7,38 @@ getWeatherButton.addEventListener("click", () => {
   const API_URL = `http://api.weatherapi.com/v1/current.json?key=${API_Key}&q=${city}`;
   function getWeather() {
     fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        function displayWeather() {
+          const { current, location } = data;
+          const weatherHtml = `
+                  <h2>Weather in ${location.name}</h2>
+                  <p>Temperature: ${current.temp_c}°C</p>
+                  <p>Condition: ${current.condition.text}</p>
+              `;
+          weatherResult.innerHTML = weatherHtml;
+          console.log(data);
+        }
+        displayWeather();
+      })
       .catch((error) => {
         console.error("Error: ", error);
       });
   }
-  console.log(city);
+  //   function displayWeather(data) {
+  //     const { main, weather, name } = data;
+  //     const weatherHtml = `
+  //           <h2>Weather in ${name}</h2>
+  //           <p>Temperature: ${main.temp}°C</p>
+  //           <p>Condition: ${weather[0].description}</p>
+  //       `;
+  //     weatherResult.innerHTML = weatherHtml;
+  //     console.log(data.name);
+  //   }
   getWeather();
 });
